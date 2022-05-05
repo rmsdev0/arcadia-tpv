2bb3837ccae6eb3f35fd1f91a3208bc6da277983
rmsdev0 <rmsdev0@gmail.com>
2022-05-05 16:26:22 -0500

    testing

--- functions/customerAdd.js
+++ functions/customerAdd.js
@@ -1,8 +1,47 @@
+const mysql = require("mysql");
+
 exports.handler = async function(context, event, callback) {
+    context.callbackWaitsForEmptyEventLoop = false;
+    const config = {
+        host: context.host,
+        port: context.port,
+        user: context.user,
+        password: context.password,
+        database: context.database
+    };
 
-    let response = new Twilio.Response();
+    console.log("connected", config);
+    try {
+        const db = new Database(config);
 
-    response.setStatusCode(200)
-    callback(null, response);
-
+        db.connection.connect();
+        const users = await db.query("select * from users");
+        await db.close();
+        console.log(users);
+        callback(null, users);
+    } catch (e) {
+        callback(e);
+    }
 };
+
+class Database {
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err) return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err) return reject(err);
+                resolve();
+            });
+        });
+    }
+}
