04ce5c182a05ccea070f5817b3dcdefad2e65f87
rmsdev0 <rmsdev0@gmail.com>
2022-05-10 19:51:49 -0500

    start api functions

--- functions/customerAdd.js
+++ functions/customerAdd.js
@@ -1,31 +1,66 @@
 const mysql = require("mysql");
 
 exports.handler = async function(context, event, callback) {
-    context.callbackWaitsForEmptyEventLoop = false;
-    const config = {
-        host: context.host,
-        port: context.port,
-        user: context.user,
-        password: context.password,
-        database: context.database
-    };
+        console.log('debug customer add called ')
+        // todo how are we going to validate this request.
+        console.log('debug eval token ', event.token)
 
-    console.log("connected", config);
-    try {
-        const db = new Database(config);
-        db.connection.connect();
-        const user = {
-            phone_number: "+12222222222",
-            first_name: "John",
-            last_name: "Doe"
+        context.callbackWaitsForEmptyEventLoop = false;
+
+        const config = {
+            host: context.host,
+            port: context.port,
+            user: context.user,
+            password: context.password,
+            database: context.database
         };
-        const users = await db.query("insert into users set ?", user);
-        await db.close();
-        console.log(users);
-        callback(null, users);
-    } catch (e) {
-        callback(e);
-    }
+
+        let response = new Twilio.Response();
+        console.log(" debug connecting ", config);
+
+        try {
+            console.log('valid token ', event.token)
+            console.log('env token ', context.vaildator)
+            console.log('token eval')
+            if (event.token === context.vaildator) {
+            const db = new Database(config);
+            db.connection.connect();
+            const prospect_record = {
+                customer_phone: event.customerPhone,
+                customer_first_name: event.customerFirstName,
+                customer_last_name: event.customerLastName,
+                customer_email: event.customerEmail,
+                utility_account_number: event.utilityAccountNumber,
+                utility_name: event.utilityName,
+                reference_number: event.referenceNumber,
+                rep_id: event.repId,
+                rep_phone: event.repPhone,
+                savings_value: event.savingsValue,
+                billing_type: event.billingType
+            };
+            const users = await db.query("insert into prospect_records set ?", prospect_record);
+            await db.close();
+            console.log("debug save complete ", users);
+
+            response.setStatusCode(200);
+            response.setBody('success')
+
+            callback(null, response);
+            }else{
+                response.setStatusCode(403);
+                response.setBody("invalid token ")
+                console.log('invalid token ', event.token)
+                callback(null, response);
+            }
+
+        } catch (e) {
+
+            response.setStatusCode(501);
+            response.setBody("error adding record")
+
+            callback(e, response);
+        }
+
 };
 
 class Database {
